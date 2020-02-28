e7f49617eb6ced6fa1ea50743f2aeb4e7e0072b4
brenorbt <brenorbt@yandex.ru>
2020-02-28 23:23:34 +0400

    Step-2

--- src/info.js
+++ src/info.js
@@ -5,11 +5,10 @@ export default () => {
     commander
         .version('0.1.0')
         .description('Compares two configuration files and shows a difference.')
-        //.option('-f, --format [type]', 'Output format')
-        //.arguments('<firstConfig> <secondConfig>');
+        .option('-f, --format [type]', 'Output format')
+        .arguments('<firstConfig> <secondConfig>');
 
     commander
-        //.command('*')
         .action(() => console.log('app is running'));
 
     commander.parse(process.argv);
