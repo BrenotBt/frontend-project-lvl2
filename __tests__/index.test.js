ab6dc18690f43202d328c7a2afef531efef2d089
brenorbt <brenorbt@yandex.ru>
2020-03-05 13:46:45 +0400

    Step 7

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,21 +4,22 @@ import path from 'path';
 import genDiff from '../src';
 
 const workFormats = ['json', 'yml', 'ini'];
+const fixtures = path.resolve(__dirname, '__fixtures__');
 
 const getFiles = (formats) => _.map(formats, (item) => [
   item,
-  path.resolve(__dirname, `__fixtures__/before.${item}`),
-  path.resolve(__dirname, `__fixtures__/after.${item}`),
+  path.resolve(fixtures, `before.${item}`),
+  path.resolve(fixtures, `after.${item}`),
 ]);
 
 const getResult = (format) => {
-  const resultPath = path.resolve(__dirname, `__fixtures__/${format}-result.txt`);
+  const resultPath = path.resolve(fixtures, `${format}-result.txt`);
   return fs.readFileSync(resultPath, 'utf8');
 };
 
 test.each(getFiles(workFormats))(
-  'genDiff',
+  'genDiff: %s',
   (format, beforePath, afterPath) => {
-    expect(genDiff(beforePath, afterPath)).toBe(getResult(format));
+    expect(genDiff(beforePath, afterPath)).toEqual(getResult(format));
   },
 );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import parser from './parsers';
+import format from './formatters';
+import types from './types';
 
 const getData = (item) => {
   let filepath = path.resolve(item);
@@ -14,23 +16,56 @@ const getData = (item) => {
   return parser(data, type);
 };
 
+const build = (object1, object2) => {
+  const keys1 = _.keys(object1);
+  const keys2 = _.keys(object2);
+  const keys = _.union(keys1, keys2);
+
+  const result = _.map(keys, (key) => {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (!_.has(object1, key)) {
+      return { key, value: value2, type: types.added };
+    }
+
+    if (!_.has(object2, key)) {
+      return { key, value: value1, type: types.deleted };
+    }
+
+    if (value1 === value2) {
+      return { key, value: value1, type: types.unchanged };
+    }
+
+    if (value1 !== value2) {
+      if (_.isObject(value1) && _.isObject(value2)) {
+        return {
+          key,
+          oldValue: value1,
+          newValue: value2,
+          type: types.nested,
+          children: build(value1, value2),
+        };
+      }
+
+      return {
+        key,
+        oldValue: value1,
+        newValue: value2,
+        type: types.changed,
+      };
+    }
+
+    return null;
+  });
+
+  return result;
+};
+
 export default (firstConfig, secondConfig) => {
   const object1 = getData(firstConfig);
   const object2 = getData(secondConfig);
-  const keys = _.union(_.keys(object1), _.keys(object2));
 
-  const str = _.map(keys, (item) => {
-    if (object1[item] === object2[item]) {
-      return `    ${item}: ${object1[item]}\n`;
-    }
-    if (_.has(object1, item) && !_.has(object2, item)) {
-      return `  - ${item}: ${object1[item]}\n`;
-    }
-    if (!_.has(object1, item) && _.has(object2, item)) {
-      return `  + ${item}: ${object2[item]}\n`;
-    }
-    return `  + ${item}: ${object2[item]}\n  - ${item}: ${object1[item]}\n`;
-  }).join('');
-
-  return `\n{\n${str}}`;
+  const result = build(object1, object2);
+  return format(result);
 };
