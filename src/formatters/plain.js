d5a671e06c699a7b17cbdffb49c587191b1c8072
brenorbt <brenorbt@yandex.ru>
2020-03-05 16:05:00 +0400

    plain.js fix2

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,21 +6,19 @@ const getValue = (value) => (_.isObject(value) ? '[complex value]' : value);
 const format = (ast, parentKey = '') => _.map(ast, (node) => {
   const currentKey = parentKey !== '' ? `${parentKey}.${node.key}` : node.key;
   let result = null;
-  switch (node.type) {
-    case types.unchanged:
-      result = null;
-      break;
-    case types.deleted:
-      result = `Property '${currentKey}' was ${node.type}`;
-      break;
-    case types.added:
-      result = `Property '${currentKey}' was ${node.type} with value: ${getValue(node.value)}`;
-      break;
-    case types.changed:
-      result = `Property '${currentKey}' was ${node.type}. From ${getValue(node.oldValue)} to ${getValue(node.newValue)}`;
-      break;
-    default:
-      result = _.flatten(format(node.children, `${parentKey !== '' ? `${parentKey}.` : ''}${node.key}`));
+  if (node.type === types.unchanged) {
+    result = null;
+  } else
+  if (node.type === types.deleted) {
+    result = `Property '${currentKey}' was ${node.type}`;
+  } else
+  if (node.type === types.added) {
+    result = `Property '${currentKey}' was ${node.type} with value: ${getValue(node.value)}`;
+  } else
+  if (node.type === types.changed) {
+    result = `Property '${currentKey}' was ${node.type}. From ${getValue(node.oldValue)} to ${getValue(node.newValue)}`;
+  } else {
+    result = _.flatten(format(node.children, `${parentKey !== '' ? `${parentKey}.` : ''}${node.key}`));
   }
   return result;
 });
