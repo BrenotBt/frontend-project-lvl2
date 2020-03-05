a48a678a01a8d5e5c688a354d8c7ca5f307b4a63
brenorbt <brenorbt@yandex.ru>
2020-03-05 16:21:42 +0400

    plain.js modif5

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,20 +1,18 @@
 import _ from 'lodash';
-import types from '../types';
 
 const getValue = (value) => (_.isObject(value) ? '[complex value]' : value);
 
 const format = (ast, parentKey = '') => _.map(ast, (node) => {
   const currentKey = parentKey !== '' ? `${parentKey}.${node.key}` : node.key;
-  let result = null;
-
   const lines = {
-    unchanged: () => null,
     deleted: () => `Property '${currentKey}' was ${node.type}`,
     added: () => `Property '${currentKey}' was ${node.type} with value: ${getValue(node.value)}`,
     changed: () => `Property '${currentKey}' was ${node.type}. From ${getValue(node.oldValue)} to ${getValue(node.newValue)}`,
     nested: () => _.flatten(format(node.children, `${parentKey !== '' ? `${parentKey}.` : ''}${node.key}`)),
   };
-
+  if (node.type === 'unchanged') {
+    return null;
+  }
   return lines[node.type]();
 });
 
